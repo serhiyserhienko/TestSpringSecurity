e020c81aa25782bd367040b0350ed293e7a42b2c
serhiy <serhiy>
2018-10-02 18:19:43 +0300

    initial commit

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class HelloController {
+
+    @GetMapping("/get")
+    public ResponseEntity<?> get(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
